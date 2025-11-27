e1358f83badcc21603142a9865cfe704dfa1a6ef
Calm Compiler <calmcompiler@gmail.com>
2025-11-27 19:22:46 +0530

    Refactor build script for Azure Functions
    Updated build script to improve functionality and structure.

--- azure-function-ts/build.sh
+++ azure-function-ts/build.sh
@@ -1,18 +1,22 @@
+#!/bin/bash -xe
 
-rm -rf dist
-mkdir dist
+rm -rf dist function.zip
+mkdir -p dist
 
 npm install
 npx tsc
 
-for folder in hello*/; do
-    mkdir -p dist/"$folder"
-    cp "$folder"*.js "$folder"*.json dist/"$folder"/ 2>/dev/null
+for func in hello*
+do
+  cp $func/function.json dist/$func/
 done
 
-cp host.json package.json dist/
+cp host.json dist/
+cp package.json dist/
 
 cd dist
-zip -r ../function.zip *
+zip -r ../function.zip .
 cd ..
+
+echo "function.zip created successfully!"
 unzip -l function.zip
