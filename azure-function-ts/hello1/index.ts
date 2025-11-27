8afb62abea81f45f2087749f1a7f568c81464133
Calm Compiler <calmcompiler@gmail.com>
2025-11-27 19:20:38 +0530

    Update response body in hello1 function

--- azure-function-ts/hello1/index.ts
+++ azure-function-ts/hello1/index.ts
@@ -3,7 +3,7 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.res = {
         status: 200,
-        body: "hello one"
+        body: "Hello from hello1 function app!"
     };
 };
 
