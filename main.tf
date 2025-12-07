b05df3807f099d76d9d8443106c13f24bdeaf883
Calm Compiler <calmcompiler@gmail.com>
2025-12-07 11:28:39 +0530

    Add blob storage container variable and update function zip
    Added a new variable for blob storage container name and updated the function zip name to include a timestamp. Adjusted the blob storage upload and function app settings to use the new variable.

--- main.tf
+++ main.tf
@@ -29,10 +29,21 @@ variable "service_plan" {
   default = "calmccserviceplan"
 }
 
+variable "blob_storage_container" {
+  description = "Blob Storage Container Name"
+  type = string
+  default = "calmccblobstore"
+}
 
-########################################################################################
+locals {
+  function_zip_timestamp = formatdate("YYYYMMDD_hhmmss", timestamp())
+  function_zip_name      = "function_${local.function_zip_timestamp}.zip"
+}
 
+
+############################################
 # Terraform Backend (State Storage)
+############################################
 terraform {
   backend "azurerm" {
     resource_group_name  = "calmccrg"
@@ -48,29 +59,29 @@ provider "azurerm" {
   tenant_id       = "6f753707-3acb-427a-8c54-ae9256022fbb"
 }
 
-
-########################################################################################
-
-
+############################################
 # Use Existing Storage Account for Function
+############################################
 data "azurerm_storage_account" "sa" {
   name                = var.storage_account
   resource_group_name = var.resource_group
+}
 
-
-# Use Existing Service Plan
+############################################
+# Use Existing Service Plan (Linux, B1)
+############################################
 data "azurerm_service_plan" "plan" {
   name                = var.service_plan
   resource_group_name = var.resource_group
 }
 
 
-########################################################################################
-
-# Build ZIP locally
+############################################
+# Build ZIP locally (Git Bash)
+############################################
 resource "null_resource" "build_function" {
   provisioner "local-exec" {
-    interpreter = ["bash", "-c"]  # Force Bash
+    interpreter = ["bash", "-c"]
     command = <<EOT
       pushd azure-function-ts
       chmod +x build.sh
@@ -84,23 +95,23 @@ resource "null_resource" "build_function" {
   }
 }
 
-
-########################################################################################
-
-# Upload ZIP to Blob Storage
+############################################
+# Upload ZIP to Blob Storage (Timestamped)
+############################################
 resource "azurerm_storage_blob" "function_zip" {
-  name                   = "function.zip"
+  name                   = local.function_zip_name
   storage_account_name   = data.azurerm_storage_account.sa.name
-  storage_container_name = "calmccblobstore"  # Must exist already
+  storage_container_name = var.blob_storage_container
   type                   = "Block"
   source                 = "${path.module}/azure-function-ts/function.zip"
 
   depends_on = [null_resource.build_function]
 }
 
-########################################################################################
 
+############################################
 # Generate SAS Token for ZIP
+############################################
 data "azurerm_storage_account_sas" "sas" {
   connection_string = data.azurerm_storage_account.sa.primary_connection_string
   https_only        = true
@@ -135,20 +146,18 @@ data "azurerm_storage_account_sas" "sas" {
 }
 
 
-########################################################################################
-
-
+############################################
 # Random suffix for unique Function App name
+############################################
 resource "random_string" "suffix" {
   length  = 5
   special = false
   upper   = false
 }
 
-########################################################################################
-
-
+############################################
 # Function App (Linux + Node16 + V3)
+############################################
 resource "azurerm_linux_function_app" "func" {
   name                = "calmccfnappv3-${random_string.suffix.result}"
   location            = var.location
@@ -164,7 +173,7 @@ resource "azurerm_linux_function_app" "func" {
 
   app_settings = {
     FUNCTIONS_WORKER_RUNTIME       = "node"
-    WEBSITE_RUN_FROM_PACKAGE = "https://${data.azurerm_storage_account.sa.name}.blob.core.windows.net/${var.blob_store}/function.zip?${data.azurerm_storage_account_sas.sas.sas}"
+    WEBSITE_RUN_FROM_PACKAGE       = "https://${data.azurerm_storage_account.sa.name}.blob.core.windows.net/${var.blob_store}/${local.function_zip_name}?${data.azurerm_storage_account_sas.sas.sas}"
     WEBSITE_NODE_DEFAULT_VERSION   = "~18"
     SCM_DO_BUILD_DURING_DEPLOYMENT = "false"
     FUNCTIONS_EXTENSION_VERSION  = "~3"
@@ -176,5 +185,3 @@ resource "azurerm_linux_function_app" "func" {
 
   depends_on = [azurerm_storage_blob.function_zip]
 }
-
-########################################################################################
