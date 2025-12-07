603689e677507d5c828762d8437c6320df1b39f2
Calm Compiler <calmcompiler@gmail.com>
2025-12-07 13:10:41 +0530

    Update azure-bootstrap.cmd

--- azure-bootstrap.cmd
+++ azure-bootstrap.cmd
@@ -30,25 +30,22 @@ REM -----------------------------------------------
 REM 3️⃣ Create Resource Group
 REM -----------------------------------------------
 echo Creating Resource Group...
-az group create ^
-  --name %RESOURCE_GROUP% ^
-  --location %LOCATION%
+az group create --name %RESOURCE_GROUP% --location %LOCATION%
+
 
 REM -----------------------------------------------
 REM 4️⃣ Create Storage Account
 REM -----------------------------------------------
 echo Creating Storage Account...
-az storage account create ^
-  --name %STORAGE_ACCOUNT% ^
-  --resource-group %RESOURCE_GROUP% ^
-  --location %LOCATION% ^
-  --sku Standard_LRS
+az storage account create --name %STORAGE_ACCOUNT% --resource-group %RESOURCE_GROUP% --location %LOCATION% --sku Standard_LRS
 
 REM -----------------------------------------------
 REM 5️⃣ Fetch Storage Account Access Key
 REM -----------------------------------------------
+setlocal EnableDelayedExpansion
+
 echo Fetching Storage Account Key...
-FOR /F "tokens=*" %%i IN ('az storage account keys list --resource-group %RESOURCE_GROUP% --account-name %STORAGE_ACCOUNT% --query [0].value -o tsv') DO (
+FOR /F "delims=" %%i IN ('az storage account keys list --resource-group %RESOURCE_GROUP% --account-name %STORAGE_ACCOUNT% --query [0].value -o tsv') DO (
   set ACCOUNT_KEY=%%i
 )
 
@@ -58,28 +55,23 @@ REM -----------------------------------------------
 
 REM Terraform State Container
 echo Creating Terraform state container...
-az storage container create ^
-  --name %TF_STATE_STORE% ^
-  --account-name %STORAGE_ACCOUNT% ^
-  --account-key %ACCOUNT_KEY%
+az storage container create --name %TF_STATE_STORE% --account-name %STORAGE_ACCOUNT% --account-key !ACCOUNT_KEY!
+
+
 
 REM Application Artifact Container
 echo Creating application artifact container...
-az storage container create ^
-  --name %BLOB_STORE% ^
-  --account-name %STORAGE_ACCOUNT% ^
-  --account-key %ACCOUNT_KEY%
+az storage container create --name %BLOB_STORE% --account-name %STORAGE_ACCOUNT% --account-key !ACCOUNT_KEY!
+
+endlocal
 
 REM -----------------------------------------------
 REM 7️⃣ Create Linux App Service Plan
 REM -----------------------------------------------
 echo Creating Linux App Service Plan...
-az appservice plan create ^
-  --name %SERVICE_PLAN% ^
-  --resource-group %RESOURCE_GROUP% ^
-  --location %LOCATION% ^
-  --sku B1 ^
-  --is-linux
+az appservice plan create --name %SERVICE_PLAN% --resource-group %RESOURCE_GROUP% --location %LOCATION% --sku B1 --is-linux
+
+
 
 REM -----------------------------------------------
 REM ✅ Completed
